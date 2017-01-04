fbdfe5c4809a2d67923eec3db2e566f5a2107599
you21979 <you2197901@gmail.com>
2017-01-04 21:15:47 +0900

    mod

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 exports.HDWallet = require('./lib/hdwallet');
 exports.TXBuilder = require('./lib/txbuilder');
-exports.HDTXBuilder = require('./lib/hdtxbuilder');
 exports.networks = require('./lib/networks');
 exports.mnemonic = require('./lib/mnemonic');
