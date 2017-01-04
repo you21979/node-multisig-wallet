b8189e974f9cdd85ff2f3109326f2ded88f39af6
you21979 <you2197901@gmail.com>
2017-01-04 18:49:43 +0900

    Destructive change

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.HDWallet = require('./lib/hdwallet');
 exports.TXBuilder = require('./lib/txbuilder');
+exports.HDTXBuilder = require('./lib/hdtxbuilder');
 exports.networks = require('./lib/networks');
 exports.mnemonic = require('./lib/mnemonic');
