dbd755e68e01f1166ce76814fa70647af0dbd3fc
you21979 <you2197901@gmail.com>
2017-01-16 21:27:59 +0900

    add multisig txsigner

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.HDWallet = require('./lib/hdwallet');
 exports.TXBuilder = require('./lib/txbuilder');
+exports.TXSigner = require('./lib/txsigner');
 exports.networks = require('./lib/networks');
 exports.mnemonic = require('./lib/mnemonic');
