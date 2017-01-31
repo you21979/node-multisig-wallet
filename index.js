61242cbbf3bf4b184a27c586e458d34abee4c08b
you2 <you2197901@gmail.com>
2017-02-01 04:17:10 +0900

    refactor

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ exports.TXBuilder = require('./lib/txbuilder');
 exports.TXSigner = require('./lib/txsigner');
 exports.networks = require('./lib/networks');
 exports.mnemonic = require('./lib/mnemonic');
+exports.hdnode = require('./lib/util_hdnode');
+exports.multisig = require('./lib/util_multisig');
